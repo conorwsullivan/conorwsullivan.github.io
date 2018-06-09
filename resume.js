e8de591289cd395a43d9a943dec9a3cbe967cd54
Conor Sullivan <conorwsullivan@gmail.com>
2018-06-09 15:50:14 -0700

    Changing resume.js to use spaces instead of tabs

--- resume.js
+++ resume.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
-	var $bullets = $('.item-detail, .links li, .interactions li');
-	var iteration = 0;
-	var paused = true;
-	setInterval(function() {
-		if(paused) {
-			return;
-		}
-		$bullets.eq(iteration).addClass('move');
-		$bullets.eq((iteration + $bullets.length - 4) % $bullets.length).removeClass('move');
-		iteration = (iteration + 1) % $bullets.length;
+  var $bullets = $('.item-detail, .links li, .interactions li');
+  var iteration = 0;
+  var paused = true;
+  setInterval(function() {
+    if(paused) {
+      return;
+    }
+    $bullets.eq(iteration).addClass('move');
+    $bullets.eq((iteration + $bullets.length - 4) % $bullets.length).removeClass('move');
+    iteration = (iteration + 1) % $bullets.length;
 
-	}, 100);
+  }, 100);
 
-	$('.print-link').click(function() {
-		//document.title = "Conor Sullivan, JavaScript Developer";
-		window.print();
-		return false;
-	});
-	$('.pause-link').click(function() {
-		paused = !paused;
-		$('.pause-link').toggleClass('pressed', !paused);
+  $('.print-link').click(function() {
+    //document.title = "Conor Sullivan, JavaScript Developer";
+    window.print();
+    return false;
+  });
+  $('.pause-link').click(function() {
+    paused = !paused;
+    $('.pause-link').toggleClass('pressed', !paused);
 
-		$('h2').toggleClass('animate', !paused);
+    $('h2').toggleClass('animate', !paused);
 
-		$('.move').removeClass('move');
-		iteration = 0;
+    $('.move').removeClass('move');
+    iteration = 0;
 
-		$('.gif, .was-gif').toggleClass('gif', !paused).toggleClass('was-gif', paused);
+    $('.gif, .was-gif').toggleClass('gif', !paused).toggleClass('was-gif', paused);
 
-		return false;
-	});
+    return false;
+  });
 });
